ba9ab44e6e0bbd9513818f8e5462a5607f11b4da
Andrew Chiang <andrew.lc.chiang@gmail.com>
2015-06-08 15:08:39 -0700

    Bind adapter to the ListView

--- app/src/main/java/com/example/android/sunshine/app/MainActivityFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivityFragment.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -44,6 +45,9 @@ public class MainActivityFragment extends Fragment {
                 );
 
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ListView listView = (ListView)rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(forecastAdapter);
 
         return rootView;
     }
